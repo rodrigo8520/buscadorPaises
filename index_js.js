d26772476aa110d25cddeb7d03938130410bb344
Ro <rodrigorojasaragon@gmail.com>
2020-10-19 17:03:02 -0300

    Cambiando de js a jquery mostrarTabla

--- index_js.js
+++ index_js.js
@@ -1,11 +1,17 @@
 
-$(document).ready(function () {
+/*$(document).ready(function () {
     alert("Hola soy un mensaje");
     document.getElementById("mostrarTabla").style.display="block";
     
+});*/
+
+$(function () {
+    alert("Hola soy un mensaje");
+
+    $("#mostrarTabla").show();
 });
 
-function mostrarDiv(){
+function mostrarDiv() {
     //document.getElementById("mostrarTabla").style.display="block";
     alert("Hola estoy buscando ctm");
 }   
