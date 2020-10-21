c1d1c0d6d2d8db17f7ed6e6f0f54bae668f985ac
Ro <rodrigorojasaragon@gmail.com>
2020-10-21 17:53:57 -0300

    btn buscar fila

--- index_js.js
+++ index_js.js
@@ -15,5 +15,25 @@ function mostrarDiv() {
     
     alert("Hola estoy buscando ctm");
     $("tbody").empty();
+   
+   var tr ='<tr><td>'+"1"+'</td>  <td>'+"2"+'</td>  <td>'+"3"+'</td>  <td>'+"4"+'</td></tr>';
+   $("tbody").append(tr);
+
+   // $("#btnBuscar").click(function(){
+
+   /*     var add = "<tr>" 
+            add+= "<td> </td>"
+            add+= "<td> </td>"
+            add+= "<td> </td>"
+            add+= "<td> </td>"
+            add+= "</tr>"
+    */
+            
+   // })
+
 }   
+
+
+       
+
 
