133da6a6a960e188834da6892c5bdffbd911d26a
Ro <rodrigorojasaragon@gmail.com>
2020-10-21 12:37:24 -0300

    Modificando js tbody

--- index_js.js
+++ index_js.js
@@ -12,26 +12,8 @@ $(function () {
 });
 
 function mostrarDiv() {
-    //document.getElementById("mostrarTabla").style.display="block";
+    
     alert("Hola estoy buscando ctm");
+    $("tbody").empty();
 }   
 
-function abla() {
-    
-    $("#mostrarTabla").hide();
-} 
-/*$(document).ready(function () {
-    $('#btnBuscar').on('click', function () {
-        $('#tabla tbody').empty()
-    });
-});*/
-
-$(function(){
-    var caca = function(){
-    $("#mostrarTabla").hide();   
-    };
-
-    var btn = document.getElementById('btnBuscar');
-    btn.addEventListener("click", caca);
-    
-})
