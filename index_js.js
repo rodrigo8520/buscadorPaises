2835f7a00f7aa9d33a2727a9217009dee6e5e5ac
Ro <rodrigorojasaragon@gmail.com>
2020-10-30 11:57:42 -0300

    ajax

--- index_js.js
+++ index_js.js
@@ -9,6 +9,7 @@ $(function () {
     alert("Hola soy un mensaje");
 
     $("#mostrarTabla").show();
+    
 });
 
 function mostrarDiv() {
@@ -20,12 +21,42 @@ function mostrarDiv() {
    var tr ='<tr><td>'+"1"+'</td>  <td>'+"2"+'</td>  <td>'+"3"+'</td>  <td>'+"4"+'</td></tr>';
    $("tbody").append(tr);
     // Para agregar varias lineas 
-   $("tbody").before(tr, tr, tr);
+   //$("tbody").before(tr, tr, tr);
    
-
+   $("tbody tr").html('<table><tr><td>1</td><td>2</td><td>3</td> <td>4</td></tr></table>');
+    var $tablaBody = $("#tablaBody");
 }   
 
+jQuery.ajax({
+	url: "https://restcountries.eu/rest/v2/name/aruba?fullText=true",
+	type: "GET",
+	async:false,
+	data: {
+		
+		accion: function(data){
+			var $tablaBody = $('#tablaBody');
+			$.each(data, function(i, item){
+				$tablaBody.append('<table><tr><td>'+ item.1 +'</td><td>'+ item.2 +'</td><td>'+ item.3 +'</td> <td>'+ item.4 +'</td></tr></table>');
+			})
+		}
 
-       
 
+		/*name: "Aruba",
+		callingCodes: "297",
+		capital: "Oranjestad",
+		region: "Americas",*/
+					
+	},
+	error: function(jqXHR, textStatus, errorThrown){ 
+			//alert("jqXHR.status: "+ jqXHR.status + "- Error Cod. 442"); 
+			retorno = ""; 
+			sin_error = false;
+			
+	},
+	success: function(regreso){
+		$("#tablaBody").text(JSON.name).append(tr)
+		//alert(regreso);
+					
+	}
+});
 
